121ff24c91b988000cddef271b900a0dacb963bf
Jack Shaw <73115676+js1294@users.noreply.github.com>
2021-11-03 00:42:40 +0000

    Delete checkfields.js

--- checkfields.js
+++
@@ -1,6 +0,0 @@
-if (document.getElementById("name").innerHTML == ""){
-    alert(document.getElementById("name").innerHTML);
-}else{
-    alert(document.getElementById("name").innerHTML);
-}
-alert("hmm..");
